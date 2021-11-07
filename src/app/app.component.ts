8f85e9ea58e4397307563a564a84f0995533a281
kookiedoodle5 <khushigandhi026@gmail.com>
2021-11-06 23:49:51 -0400

    Uploaded first image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'khushis-doodles';
+  imagesInfo: any = [];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.httpClient.get("assets/info.json").subscribe(data =>{
+      console.log(data);
+      this.imagesInfo = data;
+    })
+  }
 }
